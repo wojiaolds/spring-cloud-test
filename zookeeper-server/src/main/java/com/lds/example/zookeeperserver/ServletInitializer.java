b79178e5ae638698254e1811842cd5abd0e60530
wojiaolds <1198986674@qq.com>
2019-06-17 13:44:01 +0800

    zookeeper

---
+++ zookeeper-server/src/main/java/com/lds/example/zookeeperserver/ServletInitializer.java
@@ -0,0 +1,17 @@
+package com.lds.example.zookeeperserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @Author: lds
+ * @Date: 2019/6/17 10:40
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure( SpringApplicationBuilder application) {
+		System.out.println ("-------------ServletInitializer--------------");
+		return application.sources(ZookeeperServerApplication.class);
+	}
+}
