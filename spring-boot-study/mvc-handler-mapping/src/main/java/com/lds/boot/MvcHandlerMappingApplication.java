bdaabf55f6eb1ff26646d976579515fbfb4ae1a1
wojiaolds <1198986674@qq.com>
2019-07-19 18:03:38 +0800

    test

---
+++ spring-boot-study/mvc-handler-mapping/src/main/java/com/lds/boot/MvcHandlerMappingApplication.java
@@ -0,0 +1,13 @@
+package com.lds.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcHandlerMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcHandlerMappingApplication.class, args);
+	}
+
+}
---
+++ spring-boot-study/mvc-handler-mapping/src/main/java/com/lds/boot/controller/Controller1.java
@@ -0,0 +1,30 @@
+package com.lds.boot.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: lds
+ * @Date: 2019/7/19 17:21
+ */
+@Controller
+public class Controller1 extends AbstractController {
+	
+	@RequestMapping (value="hello")
+	@Override
+	protected ModelAndView handleRequestInternal (
+		HttpServletRequest request, HttpServletResponse response ) throws Exception {
+		
+		ModelAndView modelAndView = new ModelAndView ();
+		modelAndView.addObject ("result","成功");
+		modelAndView.setViewName ("hello");
+		return modelAndView;
+	}
+}
